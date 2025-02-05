c87aa1fa2e6d7fe8948266f3f5480a1c37794e88
SpellerBarbosa <133154923+SpellerBarbosa@users.noreply.github.com>
2025-02-05 17:30:21 -0300

    atualizações

--- src/controllers/statusController.js
+++ src/controllers/statusController.js
@@ -2,12 +2,13 @@ import Request from "../schema/requestSchema.js";
 
 const statusController = async (req, res) =>{
     const { id } = req.body;
-    console.log(id);
+
 
     if(!id) return res.status(400).json({msg: 'não foi fornecido um id de usuarios para a busca'});
 
     try {
-        const requestUser = await Request.find({id: id});
+        const requestUser = await Request.find({userId: id});
+        console.log(requestUser)
 
         if(!requestUser) return res.status(400).json({msg: 'falha na busca das solicitações'});
 
